23abe15fd00e2282f22466f9cd6b182fc274655d
arkhi <164955430+arkhiwarkhi@users.noreply.github.com>
2024-05-05 16:29:10 +0100

    Add files via upload

---
+++ install-pipewire.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0]; then
+	echo "Please run this script with sudo, doas, or as root."
+	exit 1
+fi
+
+echo "Installing pipewire-audio.."
+
+apt-get install -y "pipewire-audio"
+
+if [ -x /sbin/openrc ]; then
+	rc-update del pulseaudio-enable-autospawn
+elif [ -x /sbin/init ]; then
+	update-rc.d -f pulseaudio-enable-autospawn remove
+else
+	echo "Init system not supported at this time"
+fi
+
+rm /etc/init.d/pulseaudio-enable-autospawn/*
+
+rmdir /etc/init.d/pulseaudio-enable-autospawn
+
+mv wire ..
+
+mv wire.desktop ../.config/autostart
+
+echo "Finished installing pipewire!"
