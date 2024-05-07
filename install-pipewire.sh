966b48fdcf9c8d34dd0e8e10a9d5c3622783d64a
arkhi <164955430+arkhiwarkhi@users.noreply.github.com>
2024-05-07 21:42:58 +0100

    Update install-pipewire.sh

--- install-pipewire.sh
+++ install-pipewire.sh
@@ -1,40 +1,104 @@
 #!/usr/bin/env bash
 
+preferences_file="/etc/apt/preferences.d/pulseaudio-block.pref"
+block_pulse="Package: pulseaudio
+Pin: release o=Devuan
+Pin-Priority: -10"
+
 if [ "$EUID" -ne 0]; then
 	echo "Please run this script with sudo, doas, or as root."
 	exit 1
 fi
 
 chmod +x wire
-chmod +x wire.desktop
-
-echo "Installing pipewire-audio.."
-apt-get update -y
-apt-get install -y "pipewire-audio"
-
-if [ -x /sbin/openrc ]; then
-	rc-update del pulseaudio-enable-autospawn
-elif [ -x /sbin/init ]; then
-	update-rc.d -f pulseaudio-enable-autospawn remove
+chmod_status_wire_script = $?
+if [ $chmod_status_wire_script -eq 0 ]; then
+	chmod +x wire.desktop
+ 	chmod_status_wire_desktop=$?
+  	if [ $chmod_status_wire_desktop -eq 0 ]; then
+   		echo "Updating repositories..."
+     		apt-get update -y
+       		apt_status_update=$?
+	 	if [ $apt_get_status_update -eq 0 ]; then
+   			echo "Installing pipewire-audio..."
+   			apt-get install -y "pipewire-audio"
+			apt_status_install=$?
+   			if [ $apt_status_install -eq 0 ]; then
+      				echo "Installed pipewire successfully! Removing remnant pulseaudio services."
+				if [ -x /sbin/openrc ]; then
+    					rc-update del pulseaudio-enable-autospawn
+	 			elif [ -x /sbin/init ]; then
+     					update-rc.d -f pulseaudio-enable-autospawn remove
+	  			else
+      					echo "Init system not supported at this time"
+	   				exit
+	   			fi
+      			else
+				echo "Could not install pipewire-audio"
+    				exit 1
+  			fi
+     		else
+       			echo "Could not update repositories"
+	  		exit 1
+       		fi
+    	else
+     		echo "Could not change execute permissions on wire.desktop"
+       		exit 1
+     	fi
 else
-	echo "Init system not supported at this time"
+	echo "Could not change execute permissions on script 'wire'"
+ 	exit 1
 fi
 
 rm /etc/init.d/pulseaudio-enable-autospawn/*
-
-rmdir /etc/init.d/pulseaudio-enable-autospawn
-#intended dest is user home
-mv wire ..
-#intended dest is user home /.config/autostart
-mv wire.desktop ../.config/autostart
-
-#permanently block installation of pulseaudio on all release versions.
-preferences_file="/etc/apt/preferences.d/pulseaudio-block"
-
-block_pulse="Package: pulseaudio
-Pin: release o=Devuan
-Pin-Priority: -10"
-
-echo "$block_pulse" | sudo tee "$preferences_file" > /dev/null
-
-echo "Finished installing pipewire!"
+remove_status_autospawn=$?
+if [ $remove_status_autospawn -eq 0 ]; then
+	rmdir /etc/init.d/pulseaudio-enable-autospawn
+ 	removedir_status_autospawn=$?
+  	if [ $removedir_status_autospawn -eq 0 ]; then
+		mv wire $HOME
+  		move_status_wire=$?
+    		if [ $move_status_wire -eq 0 ]; then
+      			mv wire.desktop $XDG_CONFIG_HOME/autostart
+	 		move_status_desktop=$?
+    			if [ $move_status_desktop -eq 0 ]; then
+				echo "$block_pulse" | sudo tee "$preferences_file" > /dev/null
+    				block_pulse=$?
+				if [ $block_pulse -eq 0 ]; then
+    					echo "Pulseaudio has been blocked from installation! All done."
+	 			else
+     					echo "Unable to block installation of Pulseaudio."
+	  				exit 1
+	  			fi
+	  		else
+     				echo "XDG_CONFIG_HOME not set, trying to accommodate..."
+				${XDG_CONFIG_HOME:=$HOME/.config}
+    				mv wire.desktop $XDG_CONFIG_HOME/autostart
+				move_status_retry_desktop=$?
+    				if [ $move_status_retry_desktop -eq 0 ]; then
+					echo "XDG_CONFIG_HOME found."
+     					echo "$block_pulse" | sudo tee "$preferences_file" > /dev/null
+	  				block_pulse = $?
+       					if [ $block_pulse -eq 0 ]; then
+						echo "Pulseaudio has been blocked from installation! All done."
+	 				else
+						echo "Unable to block installation of Pulseaudio."
+      						exit 1
+	  				fi
+     					
+   				else
+					echo "Unable to set an XDG_CONFIG_HOME."
+     					exit 1
+       				fi
+			fi
+		else
+			echo "Could not move wire to '$HOME'."
+    		fi
+    	else
+		echo "Could not remove pulseaudio-enable-autospawn directory"
+  		exit 1
+     	fi
+else
+	echo "Unable to remove pulseaudio-enable-autospawn."
+ 	exit 1
+fi
